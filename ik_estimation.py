31c0ad33a595afb81f9d1d68cb8f3410432d9ec4
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-01-02 12:24:23 +0900

    implement dataset

--- ik_estimation.py
+++ ik_estimation.py
@@ -1,20 +1,39 @@
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset
 
 
-class Net(nn.Module):
-    dof = 4
+class IKDataset(Dataset):
+    def __init__(self, kinematics_pose_csv, joint_states_csv):
+        kinematics_pose = pd.read_csv(kinematics_pose_csv)
+        joint_states = pd.read_csv(joint_states_csv)
+        input_ = kinematics_pose.iloc[:, 8:12].values
+        output = joint_states.iloc[:, 3:10].values
+
+        self.input_ = torch.tensor(input_, dtype=torch.float32)
+        self.output = torch.tensor(output, dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.output)
+
+    def __getitem__(self, index):
+        return self.input_[index], self.output[index]
+
+
+class IKNet(nn.Module):
     pose = 7
+    dof = 4
     hidden_units = [200, 150, 100, 50]
 
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(self.dof, self.hidden_units[0])
+        super().__init__()
+        self.fc1 = nn.Linear(self.pose, self.hidden_units[0])
         self.fc2 = nn.Linear(self.hidden_units[0], self.hidden_units[1])
         self.fc3 = nn.Linear(self.hidden_units[1], self.hidden_units[2])
         self.fc4 = nn.Linear(self.hidden_units[2], self.hidden_units[3])
-        self.fc5 = nn.Linear(self.hidden_units[3], self.pose)
+        self.fc5 = nn.Linear(self.hidden_units[3], self.dof)
 
     def forward(self, x):
         for layer in [self.fc1, self.fc2, self.fc3, self.fc4]:
@@ -23,5 +42,5 @@ class Net(nn.Module):
 
 
 if __name__ == "__main__":
-    net = Net()
+    net = IKNet()
     print(net)
