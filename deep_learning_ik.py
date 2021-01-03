f1a5ac407062f1b473133f02de58835893bc7981
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-01-03 13:41:28 +0900

    add test

---
+++ deep_learning_ik.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+
+class IKDataset(Dataset):
+    def __init__(self, kinematics_pose_csv, joint_states_csv):
+        kinematics_pose = pd.read_csv(kinematics_pose_csv)
+        joint_states = pd.read_csv(joint_states_csv)
+        input_ = kinematics_pose.iloc[:, 3:10].values
+        output = joint_states.iloc[:, 8:12].values
+        self.input_ = torch.tensor(input_, dtype=torch.float32)
+        self.output = torch.tensor(output, dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.output)
+
+    def __getitem__(self, index):
+        return self.input_[index], self.output[index]
+
+
+class IKNet(nn.Module):
+    pose = 7
+    dof = 4
+    hidden_units = [400, 300, 200, 100, 50]
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(self.pose, self.hidden_units[0])
+        self.fc2 = nn.Linear(self.hidden_units[0], self.hidden_units[1])
+        self.fc3 = nn.Linear(self.hidden_units[1], self.hidden_units[2])
+        self.fc4 = nn.Linear(self.hidden_units[2], self.hidden_units[3])
+        self.fc5 = nn.Linear(self.hidden_units[3], self.hidden_units[4])
+        self.fc6 = nn.Linear(self.hidden_units[4], self.dof)
+
+    def forward(self, x):
+        for layer in [self.fc1, self.fc2, self.fc3, self.fc4, self.fc5]:
+            x = F.relu(layer(x))
+        return self.fc6(x)
---
+++ test.py
@@ -0,0 +1,36 @@
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+
+from deep_learning_ik import IKNet, IKDataset
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--kinematics-pose-csv", type=str, default="./kinematics_pose.csv"
+    )
+    parser.add_argument("--joint-states-csv", type=str, default="./joint_states.csv")
+    parser.add_argument("--batch-size", type=int, default=10000)
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = IKNet()
+    model.load_state_dict(torch.load("deep_learning_ik.pt"))
+    model.to(device)
+    model.eval()
+
+    dataset = IKDataset(args.kinematics_pose_csv, args.joint_states_csv)
+    test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
+
+    total_loss = 0.0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        total_loss += (output - target).norm().item() / args.batch_size
+    print(f"Total loss = {total_loss}")
+
+
+if __name__ == "__main__":
+    main()
